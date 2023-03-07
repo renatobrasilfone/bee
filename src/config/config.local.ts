e725c2615f7c5e48e5c0cedf435cfdf89a02f631
Renato Daltiba Schneider <renato@brasilfone.com.br>
2023-03-07 18:12:16 -0300

    chore: change version

---
+++ src/config/config.local.ts
@@ -0,0 +1,7 @@
+// Move credentials to backend for production.
+export const config = {
+  CLIENT_ID:      'a79759b4-8efb-46ea-8b13-b7c91b282eb1',
+  CLIENT_SECRET:  'ul6PkYfn2UpZyQvNVeUifULSIsHdBGyfrOfhD6mZLk5VsXh5Vajp',
+  CLIENT_UID:     'renato daltiba schneider',
+  BASE_TEMPLATE:  'https://rsrc.getbee.io/api/templates/m-bee'
+}
